8864bc9d1a85f604881756caec48c710b2f022e6
Joseph Watkins <joseph.o.watkins@gmail.com>
2014-04-23 12:51:08 +0100

    Refactored BattleShipGameLauncher - to BattleShipGame

--- src/main/java/battleship/BattleShipGame.java
+++ src/main/java/battleship/BattleShipGame.java
@@ -5,7 +5,7 @@ package battleship;
  * OODP 2014. Assignment Three: Battleship Game.
  * Main method - game launcher.
  */
-public class BattleShipGameLauncher {
+public class BattleShipGame {
 
     public static void main(String[] args) {
         BattleshipGameView view = new BattleshipGameViewImpl();
